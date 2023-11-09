bc82d7360ae9eca18b3212792e34870213e4291a
jeon602 <731aa@naver.com>
2023-11-09 11:46:16 +0900

    first setting & BoardController post /api/board.add 작성

---
+++ src/main/java/com/example/prj1be1109/Prj1Be1109Application.java
@@ -0,0 +1,13 @@
+package com.example.prj1be1109;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj1Be1109Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj1Be1109Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj1be1109/Prj1Be1109ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj1be1109;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj1Be1109ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

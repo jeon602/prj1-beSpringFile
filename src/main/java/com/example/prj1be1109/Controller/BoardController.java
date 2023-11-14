9dec07925ac8f72a4de3eeb304c10f949a2741a7
jeon602 <731aa@naver.com>
2023-11-14 10:16:03 +0900

    글 작성 시 작성자 확인 삭제

--- src/main/java/com/example/prj1be1109/Controller/BoardController.java
+++ src/main/java/com/example/prj1be1109/Controller/BoardController.java
@@ -18,9 +18,9 @@ public class BoardController {
 
     @PostMapping("add")
     public ResponseEntity add(@RequestBody Board board) {
-//        if (!service.validate(board)) {
-//            return ResponseEntity.badRequest().build();
-//        }
+        if (!service.validate(board)) {
+            return ResponseEntity.badRequest().build();
+        }
 
         if (service.save(board)) {
             return ResponseEntity.ok().build();

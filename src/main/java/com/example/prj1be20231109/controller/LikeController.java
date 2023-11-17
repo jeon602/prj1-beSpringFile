dba43ab1a72996dc3add30dadae290a540f85891
jeon602 <731aa@naver.com>
2023-11-17 09:29:17 +0900

    좋아요 응답 {like, countLike} 기능 추가

--- src/main/java/com/example/prj1be20231109/controller/LikeController.java
+++ src/main/java/com/example/prj1be20231109/controller/LikeController.java
@@ -8,19 +8,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/like")
 public class LikeController {
     private final LikeService service;
 
+    //
     @PostMapping
-    public ResponseEntity<Object> like(@RequestBody Like like,
-                                       @SessionAttribute(value = "login", required = false) Member login ){
-        if (login ==null){
+    public ResponseEntity<Map<String, Object>> like(@RequestBody Like like,
+                                                    @SessionAttribute(value = "login", required = false) Member login) {
+        if (login == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-        service.update(like, login);
-        return null;
+        //하트를 눌렀을 때 좋아요를 한건지 취소를 한건지 알아야 함.
+//        좋아요 개수 정보도 있어야 함
+//        {like : true,
+//        countlike: 5 ----> 좋아요 개수는? 자바빈 or map으로 넘김}
+        return ResponseEntity.ok(service.update(like, login));
     }
 }
--- src/main/java/com/example/prj1be20231109/service/LikeService.java
+++ src/main/java/com/example/prj1be20231109/service/LikeService.java
@@ -6,23 +6,29 @@ import com.example.prj1be20231109.mapper.LikeMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class LikeService {
 
     private final LikeMapper mapper;
-    public void update(Like like, Member login){
+
+    public Map<String, Object> update(Like like, Member login) {
         like.setMemberId(login.getId());
         //처음 좋아요 누를 때 insert
         // 다시 누르면 delete
         // 처음 눌렀는지 다시 눌렀는지 알아야 함.
 //        0이면 원래 없었던 것,
         int count = 0;
-        if(mapper.delete(like)==0){
+        if (mapper.delete(like) == 0) {
             count = mapper.insert(like);
         }
-        }
-        //
+        int countLike = mapper.countByBoardId(like.getBoardId());
+        return Map.of("like",count == 1,
+                "countLike", countLike);
     }
+    //
+}
 
 

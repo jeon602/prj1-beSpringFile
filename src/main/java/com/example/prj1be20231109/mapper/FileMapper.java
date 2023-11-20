d7c7413fb7cd3c9ee8b2c0d2c46e92b81bcfb039
jeon602 <731aa@naver.com>
2023-11-20 11:48:58 +0900

    파일 첨부시 boardfile에 파일 정보 insert CREATE TABLE boardFile (                                    id INT PRIMARY KEY AUTO_INCREMENT,                                    boardId INT NOT NULL REFERENCES board(id),                                    name VARCHAR(500) NOT NULL                                );

---
+++ src/main/java/com/example/prj1be20231109/mapper/FileMapper.java
@@ -0,0 +1,12 @@
+package com.example.prj1be20231109.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface FileMapper {
+    @Insert("""
+            INSERT INTO boardFile(boardId, name)
+            values  (#{boardId},#{name})""")
+    int insert(Integer boardId, String name);
+}

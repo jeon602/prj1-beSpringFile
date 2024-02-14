d9b6a36b127b9b199e90d89dc80e88a9ec5a642f
yeon <731aa@naver.com>
2024-02-14 16:30:52 +0900

    AppErrorController 오탈자 수정

--- src/main/java/com/example/prj1be20231109/controller/AppErrorController.java
+++ src/main/java/com/example/prj1be20231109/controller/AppErrorController.java
@@ -1,14 +1,31 @@
 package com.example.prj1be20231109.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppErrorController implements ErrorController {
+
     @GetMapping("/error")
-    public String haddfleError(){
-        return "/";
+    public String handleError() {
+        return "error"; // 수정된 부분: "/" -> "error"
     }
 }
+
+
+
+//package com.example.prj1be20231109.controller;
+//
+//import org.springframework.boot.web.servlet.error.ErrorController;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class AppErrorController implements ErrorController {
+//
+//    @GetMapping("/error")
+//    public String handleError() {
+//        return "error"; // 수정된 부분: "/" -> "error"
+//    }
+//}
